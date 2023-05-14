3e6ced1423999e70b6e2f1a70b2d54e8671617f9
Isaac Weingarten <34663972+isaacwein@users.noreply.github.com>
2023-05-14 16:30:13 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import testCSS from './test.css?url'
+    console.log(testCSS)
+</script>
+<svelte:head>
+    <link rel="stylesheet" href="{testCSS}">
+</svelte:head>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
